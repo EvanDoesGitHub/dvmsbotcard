099cc70de5f93448ae28d449367256faa58bf8d6
EvanDoesGitHub <76410162+EvanDoesGitHub@users.noreply.github.com>
2025-05-18 12:19:33 -0400

    Update rob.js

--- commands/rob.js
+++ commands/rob.js
@@ -1,27 +1,23 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { writeFileSync } = require('fs'); // Use writeFileSync for simplicity
-
 const ROB_CHANCE = 0.3; // 30% chance of getting caught
 const ROB_MULTIPLIER = 0.1; // Steal 10% of the target's balance
 const FINE_MULTIPLIER = 1.2; // Pay 20% more than you tried to steal
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('rob')
-        .setDescription('Attempt to steal money from another user.')
-        .addUserOption(option =>
-            option.setName('target')
-                .setDescription('The user you want to rob.')
-                .setRequired(true)),
-    async execute(interaction, { db }) { // Destructure db from the client object
-        const robberId = interaction.user.id;
-        const targetId = interaction.options.getUser('target').id;
+    name: 'rob', // Changed to name for message command
+    description: 'Attempt to steal money from another user.',
+    async execute(message, args, { db }) { // Removed interaction, added message
+        const robberId = message.author.id;
+        const targetId = message.mentions.users.first()?.id; // Get ID from mention
 
-        if (robberId === targetId) {
-            return interaction.reply("You can't rob yourself!");
+        if (!targetId) {
+            return message.reply("Please mention the user you want to rob.");
         }
 
-        await db.read(); // Ensure the database is read
+        if (robberId === targetId) {
+            return message.reply("You can't rob yourself!");
+        }
+
+        await db.read();
 
         // Initialize user balances if they don't exist
         if (!db.data.users[robberId]) {
@@ -35,7 +31,7 @@ module.exports = {
         let targetBalance = db.data.users[targetId].balance;
 
         if (targetBalance === 0) {
-            return interaction.reply("That user has no money to steal!");
+            return message.reply("That user has no money to steal!");
         }
 
         const stolenAmount = Math.floor(targetBalance * ROB_MULTIPLIER);
@@ -44,18 +40,18 @@ module.exports = {
         if (Math.random() < ROB_CHANCE) { // Robber gets caught
             db.data.users[robberId].balance -= fineAmount;
             db.data.users[targetId].balance += fineAmount;
-            await db.write(); // Use await here
+            await db.write();
 
             if (robberBalance < fineAmount) {
-                return interaction.reply(`You were caught and fined ${fineAmount}₩! You're now in debt.`);
+                return message.reply(`You were caught and fined ${fineAmount}₩! You're now in debt.`);
             } else {
-                return interaction.reply(`You were caught and fined ${fineAmount}₩!`);
+                return message.reply(`You were caught and fined ${fineAmount}₩!`);
             }
         } else { // Robber succeeds
             db.data.users[robberId].balance += stolenAmount;
             db.data.users[targetId].balance -= stolenAmount;
-            await db.write(); // and here
-            return interaction.reply(`You stole ${stolenAmount}₩ from <@${targetId}>!`);
+            await db.write();
+            return message.reply(`You stole ${stolenAmount}₩ from <@${targetId}>!`);
         }
     },
 };
